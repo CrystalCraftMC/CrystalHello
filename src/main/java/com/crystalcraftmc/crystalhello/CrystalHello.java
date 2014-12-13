68c656d971906bcd9b3fbcde84f1b63dadfe8da0
Ivan <Jacc734@users.noreply.github.com>
2014-12-12 21:00:35 -0500

    Command Outline

--- src/main/java/com/crystalcraftmc/crystalhello/CrystalHello.java
+++ src/main/java/com/crystalcraftmc/crystalhello/CrystalHello.java
@@ -26,7 +26,12 @@
 package com.crystalcraftmc.crystalhello;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public class CrystalHello extends JavaPlugin {
     public void onEnable() {
@@ -38,5 +43,37 @@ public class CrystalHello extends JavaPlugin {
     }
 
     // Insert attributes and behaviors here!
+    //So far: basic online of a few main command checks and (hopefully) some code that will determine whether player is holding ice.
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		//----------------------------COMMAND (1 of 4)-----------------------------
+		if(cmd.getName().equalsIgnoreCase("<command name> needs to be added to our (title goes here).yml file")){
+
+			//-----------------------NON-PLAYER (2 of 4)---------------------------
+			if(!(sender instanceof Player)){
+				//maybe additional statements
+				sender.sendMessage("You are not a player.");
+				return false;
+			} 
+			//------------------------PLAYER (3 of 4) [and] ICE Check (4 of 4)---------------------------
+			//Note there is no specific check for (sender instanceof Player) Just the confirmation that it is not- a non-player
+			else {
+				sender.sendMessage("You are a player.");
+				Player player = (Player) sender;
+				//if .getItemInHand().getData is equal to "ICE" Then this should work. I don't know for sure though.
+				//.getItemInHand might work by itself if .getData() has an undesired data type (effect).
+				if(player.getItemInHand().equals(Material.ICE)){
+					player.sendMessage("Your hands must be cold, after holding that ice for so long.");
+				//This line/area here is reserved for what ever happens when the command, ice in hand, and player come together
+					return true;
+				} else {
+					player.sendMessage("This command requires icy hands, not an icy heart! You need to hold some ice.");
+					return false;
+				} 
+			} 
+		}
+		return false;
+	}
     // See here(http://wiki.bukkit.org/Plugin_Tutorial) for extra help!
 }
